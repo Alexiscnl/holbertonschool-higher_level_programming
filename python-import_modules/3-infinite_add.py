2d872b0577fb5176e74714c2df62ff2ce83d99c0
Alexiscnl <alexis.cornillon@gmail.com>
2025-05-07 14:52:36 +0200

    fix: change code

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 import sys
 
-arguments = sys.argv[1:]
-total = 0
-
-for index, value in enumerate(arguments):
-    total += int(value)
-print(total)
+if __name__ == "__main__":
+    result = 0
+    for i in range(1, len(sys.argv)):
+        result += int(sys.argv[i])
+    print(result)
