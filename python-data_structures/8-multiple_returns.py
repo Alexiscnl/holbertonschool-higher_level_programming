6d421be0286f237d85cde7895db8138744e1b052
Alexiscnl <alexis.cornillon@gmail.com>
2025-05-12 16:22:18 +0200

    fix: delete else

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -2,8 +2,7 @@
 
 def multiple_returns(sentence):
     lenght = len(sentence)
-    first_chr = sentence[0]
     if len(sentence) == 0:
         return (0, None)
-    else:
-        return (lenght, first_chr)
+    first_chr = sentence[0]
+    return (lenght, first_chr)
