fd89999a2c5664544ba4bdaf5196f3fb777334fb
Alexiscnl <alexis.cornillon@gmail.com>
2025-05-12 10:50:38 +0200

    fix: add comment

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
-    for idx in my_list:
-        if idx < 0 or idx > len(my_list):
-            return None
-        return my_list[idx]
+    """
+    Récupère un élément d'une liste à un indice donné.
+    :param my_list: La liste d'éléments.
+    :param idx: L'indice de l'élément à récupérer.
+    :return: L'élément si l'indice est valide, sinon None.
+    """
+    if idx < 0 or idx >= len(my_list):
+        return None
+    return my_list[idx]
