dd2b29a7d8a8b1d9accc6b47cfc47dab430089a2
Alexiscnl <alexis.cornillon@gmail.com>
2025-05-14 12:09:04 +0200

    fix: add .format

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -4,9 +4,9 @@ def safe_print_list_integers(my_list=[], x=0):
     for i in range(x):
         try:
             if isinstance(my_list[i], int):
-                print(my_list[i], end="")
+                print("{:d}".format(my_list[i]), end="")
                 count += 1
         except (ValueError, TypeError):
-            pass
+            continue
     print()
     return count
