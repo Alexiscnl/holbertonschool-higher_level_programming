907de2c3aa78ac4690453a0d1b29d8c0c2064ef4
Alexiscnl <alexis.cornillon@gmail.com>
2025-05-15 11:13:49 +0200

    fix: delete line

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -4,7 +4,6 @@ Module for add_integer function.
 Adds two numbers, casting floats to ints.
 Raises TypeError if inputs are invalid.
 """
-
 def add_integer(a, b=98):
     """
     Adds two integers.
@@ -23,6 +22,6 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    a = int(a) if isinstance(a, float) else a
-    b = int(b) if isinstance(b, float) else b
+    a = int(a)
+    b = int(b)
     return a + b
