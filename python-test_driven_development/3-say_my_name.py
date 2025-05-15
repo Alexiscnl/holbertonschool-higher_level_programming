1bf30adeda71f0e609c3185c056d26290e82c213
Alexiscnl <alexis.cornillon@gmail.com>
2025-05-15 15:56:59 +0200

    feat: creat function write the first name and last name

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+def say_my_name(first_name, last_name=""):
+    """
+    Prints "My name is <first name> <last name>".
+
+    Args:
+        first_name (str): Le prénom.
+        last_name (str, optionnel): Le nom de famille.
+
+    Raises:
+        TypeError: Si `first_name` ou `last_name` ne sont pas des chaînes.
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    if last_name == "":
+        print(f"My name is {first_name}")
+    else:
+        print(f"My name is {first_name} {last_name}")
