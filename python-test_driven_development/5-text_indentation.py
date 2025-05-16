fb61ea4a63c65507d95f648336b724d239ee86a6
Alexiscnl <alexis.cornillon@gmail.com>
2025-05-16 11:11:41 +0200

    fix: dd commnt

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,5 +1,21 @@
 #!/usr/bin/python3
+"""
+Module 5-text_indentation
+
+This module defines the text_indentation(text) function, which displays text
+by inserting two newlines after each '.', '?', or ':'.
+The text must not begin or end with a space on any line.
+"""
 def text_indentation(text):
+    """
+    Prints a text with 2 new lines after each '.', '?' and ':'.
+
+    Args:
+        text (str): The text to indent.
+
+    Raises:
+        TypeError: If text is not a string.
+    """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
     temp = ""
