757e9a0d88e946bfe39bb1d510d4c3681129f0be
Alexiscnl <alexis.cornillon@gmail.com>
2025-05-16 09:57:21 +0200

    fix:  teste chnage code

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -15,5 +15,4 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-    full_name = f"My name is {first_name} {last_name}".strip()
-    print(full_name)
+    print("My name is {} {}".format(first_name, last_name))
