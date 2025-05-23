bacfabae59c12ff7ea22f2d761e6e6b6e35b11a3
Alexiscnl <alexis.cornillon@gmail.com>
2025-05-23 13:14:04 +0200

    feat: creat rectangle empty

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+This module defines the Rectangle class for representing
+rectangle geometric shapes. It is designed to be extended with
+attributes such as width and height in future versions.
+"""
+
+
+class Rectangle:
+    """
+    Represents an empty rectangle.
+
+    This class currently does not define any attributes or methods.
+    It serves as a basic structure to be extended.
+    """
+
+    def __init__(self):
+        """
+        Initializes a new Rectangle instance.
+
+        Currently, this does not take any parameters or perform any actions.
+        """
+        pass
