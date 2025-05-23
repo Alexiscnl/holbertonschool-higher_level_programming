d1995599722cfe48c273636aebf59d80901c9379
Alexiscnl <alexis.cornillon@gmail.com>
2025-05-23 15:49:00 +0200

    fix: change if by elif

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -42,7 +42,7 @@ class Rectangle:
         """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
@@ -70,7 +70,7 @@ class Rectangle:
         """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
 
