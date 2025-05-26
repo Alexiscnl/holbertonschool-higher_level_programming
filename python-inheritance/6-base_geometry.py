23b043e68c0da7b04a9fc94448ad8623e055d603
Alexiscnl <alexis.cornillon@gmail.com>
2025-05-26 12:12:50 +0200

    feat: add abstract area method to BaseGeometry class that raises exception

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Module 6-base_geometry
+
+Defines a BaseGeometry class with an unimplemented area method.
+"""
+
+class BaseGeometry:
+    """
+    Base class for geometry-related calculations.
+
+    Methods:
+        area(self): raises an Exception to indicate it is not yet implemented.
+    """
+
+    def area(self):
+        """
+        Raises an Exception indicating that the area method is not implemented.
+        """
+        raise Exception("area() is not implemented")
