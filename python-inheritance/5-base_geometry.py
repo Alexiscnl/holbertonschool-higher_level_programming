2c826343aec7d8648c589e340e255ace6bf4a97e
Alexiscnl <alexis.cornillon@gmail.com>
2025-05-26 11:49:31 +0200

    feat: define empty BaseGeometry class as placeholder for future geometry features

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module 5-base_geometry
+
+Defines an empty class BaseGeometry.
+"""
+
+class BaseGeometry:
+    """
+    A base class for geometry-related operations.
+
+    Currently empty and serves as a placeholder for future geometric methods.
+    """
+    pass
