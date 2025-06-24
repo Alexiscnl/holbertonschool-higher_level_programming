f6b8268629e43e3d2d974b1f0fcaac6699d20056
Alexiscnl <alexis.cornillon@gmail.com>
2025-06-24 11:12:14 +0200

    fix: add flush

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,5 @@
 -- Create DB and user_0d_2 with read-only access
-CREATE DATABASE IF NOT EXISTS hbtb_0d_2;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON hbtb_0d_2.* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
