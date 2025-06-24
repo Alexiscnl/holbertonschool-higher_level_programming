d98d403e246c9d4c6f774157284a291d329a3657
Alexiscnl <alexis.cornillon@gmail.com>
2025-06-24 10:51:54 +0200

    feat(sql): create cities table with foreign key to states in hbnb_0d_usa database

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,11 @@
+-- This script creates the database hbnb_0d_usa and the table cities.
+-- The table cities includes an auto-incremented id (PRIMARY KEY),
+-- a state_id that references the id of the states table (FOREIGN KEY),
+-- and a non-null name column.
+CREATE DATABASE IF NOT EXISTS hbnb_0d_usa;
+CREATE TABLE IF NOT EXISTS hbnb_0d_usa.cities(
+	id INT AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY,
+	state_id INT NOT NULL,
+	name VARCHAR(256) NOT NULL,
+	FOREIGN KEY (state_id) REFERENCES hbnb_0d_usa.states(id)
+);
