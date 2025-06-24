89f24ffb11e0159d43169bd370744dd3618e900b
Alexiscnl <alexis.cornillon@gmail.com>
2025-06-24 11:08:18 +0200

    fix: change hbnb by hbtn

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -2,7 +2,7 @@
 -- The table cities includes an auto-incremented id (PRIMARY KEY),
 -- a state_id that references the id of the states table (FOREIGN KEY),
 -- and a non-null name column.
-CREATE DATABASE IF NOT EXISTS hbnb_0d_usa;
+CREATE DATABASE IF NOT EXISTS hbtb_0d_usa;
 CREATE TABLE IF NOT EXISTS hbnb_0d_usa.cities(
 	id INT AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY,
 	state_id INT NOT NULL,
