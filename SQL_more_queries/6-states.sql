51cd3fa72300d8268565779d3748e10327bd5d61
Alexiscnl <alexis.cornillon@gmail.com>
2025-06-25 10:35:14 +0200

    fix: change ident and delte use

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,8 +1,7 @@
 -- This script creates the database hbtn_0d_usa and the table states.
 -- The table states contains an auto-incremented primary key `id` and a non-null `name`.
 CREATE DATABASE IF NOT EXISTS hbtb_0d_usa;
-USE hbtb_0d_usa;
-CREATE TABLE IF NOT EXISTS states(
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
 id INT AUTO_INCREMENT PRIMARY KEY,
 name VARCHAR(256) NOT NULL
 );
