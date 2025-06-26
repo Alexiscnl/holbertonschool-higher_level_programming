b3850c32fada1b4142c4edaf78402d268f197214
Alexiscnl <alexis.cornillon@gmail.com>
2025-06-26 15:38:53 +0200

    refactor: améliorer la documentation et le formatage du code

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,45 +1,24 @@
 #!/usr/bin/python3
-"""
-This script connects to a MySQL database and retrieves all rows from the 'states' table
-where the name starts with the letter 'N'. The results are ordered by the 'id' column
-in ascending order and printed to the standard output.
+"""Lists all states with a name starting with N
+from the database hbtn_0e_0_usa."""
 
-Usage:
-    ./1-filter_states.py <mysql_username> <mysql_password> <database_name>
-
-Arguments:
-    mysql_username:   The username to connect to the MySQL server.
-    mysql_password:   The password for the given MySQL username.
-    database_name:    The name of the database to use.
-
-Requirements:
-    - MySQLdb module must be installed.
-    - The script must be run with exactly three arguments.
-
-Example:
-    ./1-filter_states.py root root_password hbtn_0e_0_usa
-
-Note:
-    The script uses a simple SQL query with a LIKE clause to filter state names
-    starting with 'N'. The connection is closed after the operation.
-"""
 
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
     db = MySQLdb.connect(
-        port=3306,
         host="localhost",
+        port=3306,
         user=sys.argv[1],
         passwd=sys.argv[2],
         db=sys.argv[3]
     )
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE BINARY name LIKE 'N%' ORDER BY id ASC")
+    cur.execute(
+        "SELECT * FROM states WHERE BINARY name LIKE 'N%' ORDER BY id ASC")
     lines = cur.fetchall()
     for i in lines:
         print(i)
     cur.close()
     db.close()
-
