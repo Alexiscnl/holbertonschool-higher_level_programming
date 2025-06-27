7b070a1b06e72e026f3c2861dcb2df73626caba2
Alexiscnl <alexis.cornillon@gmail.com>
2025-06-27 11:37:39 +0200

    fix: SQL query formatting by adding a space at the end of the line

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     )
     cur = db.cursor()
     cur.execute(
-        "SELECT cities.id, cities.name, states.name FROM cities"
+        "SELECT cities.id, cities.name, states.name FROM cities "
         "JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC")
     lines = cur.fetchall()
     for i in lines:
