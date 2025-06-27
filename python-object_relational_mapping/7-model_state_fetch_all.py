13db8df7110b7a2525306d51f35f20480f3a4af7
Alexiscnl <alexis.cornillon@gmail.com>
2025-06-27 12:54:18 +0200

    fix: add variable

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -18,6 +18,7 @@ if __name__ == "__main__":
 
     Session = sessionmaker(bind=engine)
     session = Session()
-    for state in session.query(State).order_by(State.id).all():
+    states = session.query(State).order_by(State.id).all()
+    for state in states:
         print(f"{state.id}: {state.name}")
     session.close()
