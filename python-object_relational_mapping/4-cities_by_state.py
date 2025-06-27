e8e2aa4b4e2473c920f8142b99c4232a2c751e88
Alexiscnl <alexis.cornillon@gmail.com>
2025-06-27 10:43:12 +0200

    style: update docstring for clarity and improve code formatting

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Lists all cities with their corresponding state name from the database."""
+"""Lists all cities from the database
+hbtn_0e_0_usa, with their state name."""
+
 
 import MySQLdb
 import sys
@@ -10,14 +12,15 @@ if __name__ == "__main__":
         port=3306,
         user=sys.argv[1],
         passwd=sys.argv[2],
-        db=sys.argv[3]
+        db=sys.argv[3],
     )
     cur = db.cursor()
     cur.execute(
         "SELECT cities.id, cities.name, states.name FROM cities "
-        "JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC")
-    lines = cur.fetchall()
-    for i in lines:
-        print(i)
+        "JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC"
+    )
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
     cur.close()
     db.close()
