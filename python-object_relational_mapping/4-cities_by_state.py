c088e145b38df0b148d2d1c296d9ca62dc418598
Alexiscnl <alexis.cornillon@gmail.com>
2025-06-27 10:27:25 +0200

    feat: list all cities and their corresponding state from database

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Lists all cities with their corresponding state name from the database."""
+
+
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+    cur = db.cursor()
+    cur.execute(
+        "SELECT cities.id, cities.name, states.name " \
+        " FROM cities" \
+        " JOIN states ON cities.state_id = states.id" \
+        " ORDER BY cities.id")
+    lines = cur.fetchall()
+    for i in lines:
+        print(i)
+    cur.close()
+    db.close()
