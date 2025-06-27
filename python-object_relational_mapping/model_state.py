552dba624ca203719830c06a4b55e619b426ba6a
Alexiscnl <alexis.cornillon@gmail.com>
2025-06-27 12:28:53 +0200

    fix: change model

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python3
+
 """
-Defines a SQLAlchemy ORM class representing the 'states' table.
+Defines a State class and a Base instance for SQLAlchemy
 """
+
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
+
+# Create the Base instance
 Base = declarative_base()
 
+
 class State(Base):
+    """State class for the states table"""
     __tablename__ = 'states'
-    id = Column(Integer,primary_key=True, autoincrement=True, nullable=False)
+
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
