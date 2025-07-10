8780d4a86c54ffa8a68acd363ab4c7a051b8667c
Alexiscnl <alexis.cornillon@gmail.com>
2025-07-10 13:41:19 +0200

    fix: change semistandard

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const argv2 = process.argv[2];
 if (!isNaN(parseInt(argv2))) {
-	console.log('My number: ' + parseInt(argv2));
-}else {
-	console.log('Not a number');
+  console.log('My number: ' + parseInt(argv2));
+} else {
+  console.log('Not a number');
 }
