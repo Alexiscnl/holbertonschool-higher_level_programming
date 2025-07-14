c81d583967e0cfd53def7a03fc8244cf23e982b3
Alexiscnl <alexis.cornillon@gmail.com>
2025-07-14 12:37:12 +0200

    feat(js): fetch and display "hello" translation from API

---
+++ javascript-dom_manipulation/8-script.js
@@ -0,0 +1,9 @@
+const element = document.querySelector("#hello")
+
+fetch("https://hellosalut.stefanbohacek.dev/?lang=fr")
+	.then(function(response) {
+		return response.json();
+	})
+	.then(function(data) {
+		element.textContent = data.hello;
+	})
