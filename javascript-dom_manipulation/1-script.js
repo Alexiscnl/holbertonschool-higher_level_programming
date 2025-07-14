27c7a67a42c7e2aba39dc4f65170546b4e8550be
Alexiscnl <alexis.cornillon@gmail.com>
2025-07-14 09:56:01 +0200

    feat: change header color to red on click

---
+++ javascript-dom_manipulation/1-script.js
@@ -0,0 +1,6 @@
+const element = document.querySelector("#red_header");
+const header = document.querySelector("header");
+
+element.addEventListener("click", function() {
+	header.style.color ="#FF0000"
+});
